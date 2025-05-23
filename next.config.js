cadad0f5f5c746763794d8e373dd513413253ace
Fednev Ruslan <fednevr97@yandex.ru>
2025-05-23 16:22:04 +0300

    Оптимизация 4

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  // Удалите или закомментируйте experimental.serverActions если не используете
-  // experimental: {
-  //   serverActions: true,
-  // },
+
+  experimental: {
+    optimizePackageImports: ['react-icons', 'lodash-es'],
+    isrMemoryCacheSize: 50, // Уменьшите, если приложение большое
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+    reactRemoveProperties: true,
+  },
+  webpack: (config) => {
+    config.optimization.splitChunks = {
+      chunks: 'all',
+      maxSize: 244 * 1024, // 244 KB
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
